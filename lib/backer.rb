7d5c179bddd856c57d3bf4e0ff235485865db2f0
taroj khan <tarujkhan@yahoo.com>
2020-03-22 16:58:11 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,13 @@
+class Backer 
+  attr_reader :name, :backed_projects
+  
+  def initialize(name)
+    @name = name 
+    @backed_projects = []
+  end 
+  
+  def back_project(project)
+  @back_projects << project
+end 
+  
+end
